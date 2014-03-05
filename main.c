3c8bc4c963570c4cc38c4f533c0490ff7f20101b
Aaron Gross <nocookies92@yahoo.com>
2014-03-05 18:57:03 -0500

    added timing, input, curses window context

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
-/*
+/* =============================================================================
  * Aaron Gross
  * cursesProject.c
  * 3/4/14
- * V 0.1
+ * V 0.2
+ * =============================================================================
  */
 
 #include <stdio.h>
@@ -15,19 +16,46 @@
 #define Herp "Herp"
 #endif
 
+#define MAX_FPS 50
+#define MSEC_IN_SEC 1000
+#define DELAY (MSEC_IN_SEC / MAX_FPS)
+#define TICS_PER_SEC MAX_FPS
 void update();
 void render();
 void pause();
 
+int getKey();
+
+bool running = TRUE;
+int tick = 0;
+
 int main(int argc, char *argv[])
 {
-	bool running = TRUE;
+	WINDOW *w = initscr(); // init a curses window
+
+	/*
+	 * These two line make it so that getch() doesn't need to wait 
+	 * for a character to be entered for getch() to return.
+	 * This allows the game to update and render even when there is no input
+	 * Also put as: call getch() without blocking
+	 */
+	cbreak(); // put terminal in c break mode
+	nodelay(w, TRUE); // don't wait for enter to read char
+
+	noecho(); // turn off key echoing
+	keypad(w, TRUE); // allow getch() to detect non-character key presses
 
 	while(running)
 	{
 		update();
 		render();
 		pause();
+
+		tick++;
+		if(tick == TICS_PER_SEC)
+		{
+			tick = 0;
+		}
 	}
 
 	return 0;
@@ -35,16 +63,31 @@ int main(int argc, char *argv[])
 
 void update()
 {
+	int key = 0;
 
+	key = getKey();
+	if(key == 27)
+	{
+		running = FALSE;
+	}
 }
 
 void render() 
 {
+	if(tick == 0)
+	{
+ 		printw("Hello, World!\n", );
+	}
 
+	refresh(); // call curses's refresh funct to update screen
 }
 
 void pause() 
 {
-	napms(500);
- 	printf("Hello, World!\n");
+	napms(DELAY); // from curses.h, sleeps for n milliseconds
+}
+
+int getKey(){
+	int key = getch();
+	return key;
 }
