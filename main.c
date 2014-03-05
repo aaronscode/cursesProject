be1cd4bfbbc3253b550e967c64d9c1db28452d74
Aaron Gross <nocookies92@yahoo.com>
2014-03-04 22:41:19 -0500

    added #defines to determine OS, may move later

--- main.c
+++ main.c
@@ -8,6 +8,13 @@
 #include <stdio.h>
 #include <curses.h>
 
+#ifdef _WIN32		// defs to check OS
+#define Herp "Derp"     // may put in .h file later to reduce clutter
+#endif
+#ifdef __linux__
+#define Herp "Herp"
+#endif
+
 int main(int argc, char *argv[])
 {
 	bool running = TRUE;
