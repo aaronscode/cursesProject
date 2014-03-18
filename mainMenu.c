fdf574766ab8e7cfe921053837433a6b99c8931e
Aaron Gross <nocookies92@yahoo.com>
2014-03-17 23:02:02 -0400

    a

--- mainMenu.c
+++ mainMenu.c
@@ -1,18 +1,13 @@
 #define MAX_CHOICE_CHARS 30 // max number of chars a menu option can have
 
-<<<<<<< HEAD
 typedef struct
 {
-=======
-typdef struct {
->>>>>>> 594583d7fab2cc5436533d28f08eb1f8e17c087e
 	int id;
 	char text[MAX_CHOICE_CHARS];
 } menuChoice;
 
 menuChoice *mc;
 
-<<<<<<< HEAD
 int numChoices;
 int screenHeight, screenWidth;
 int titleColumns, titleRows;
@@ -38,13 +33,9 @@ bool initTitle(int columns, int rows, char text)
 
 void updateMainMenu()
 {
-
 }
 
 void cleanupMenu()
 {
 	free(mc);
 }
-=======
-void
->>>>>>> 594583d7fab2cc5436533d28f08eb1f8e17c087e
