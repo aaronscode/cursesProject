b281096976ae1c808fcd354de9d0282f92642b73
Aaron Gross <nocookies92@yahoo.com>
2014-03-17 23:04:01 -0400

    a

--- mainMenu.h
+++ mainMenu.h
@@ -1,7 +1,6 @@
 #ifndef __MAIN_MENU__
 #define __MAIN_MENU__
 
-<<<<<<< HEAD
 #include <string.h>
 #include <stdlib.h>
 
@@ -11,11 +10,6 @@ void initMenu(int numberOfChoices, int width, int height);
 bool initTitle(int columns, int rows, char text);
 void cleanupMenu();
 void updateMainMenu();
-=======
-typedef struct menuChoice;
-
-void initMenu(int numChoices);
->>>>>>> 594583d7fab2cc5436533d28f08eb1f8e17c087e
 
 #include "mainMenu.c"
 #endif
