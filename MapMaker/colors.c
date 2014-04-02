f1c0340a7d29d0dbcfc9ded01532d09a0275c10d
Aaron Gross <nocookies92@yahoo.com>
2014-04-02 16:20:01 -0400

    modified colors.c a bit

--- MapMaker/colors.c
+++ MapMaker/colors.c
@@ -30,7 +30,14 @@ int main(int argc, char *argv[])
 		}
 	}
 	color_set(0, NULL);
-	mvprintw(20,20,"%d", COLOR_RED);
+	mvprintw(16, 0, "Colors: %d", COLORS);
+	mvprintw(17, 0, "ColorPairs: %d", COLOR_PAIRS);
+	mvprintw(18, 0, "BLACK: %d", COLOR_BLACK);
+	mvprintw(19, 0, "RED: %d", COLOR_RED);
+	mvprintw(20, 0, "GREEN: %d", COLOR_GREEN);
+	mvprintw(21, 0, "YELLOW: %d", COLOR_YELLOW);
+	mvprintw(22, 0, "BLUE: %d", COLOR_BLUE);
+	mvprintw(23, 0, "MAGENTA: %d", COLOR_MAGENTA);
 	getch();
 	delwin(w);
     	endwin();
