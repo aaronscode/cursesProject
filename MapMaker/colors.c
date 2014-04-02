257a53ce490419b014fac77e605ec571707d49f3
Aaron Gross <nocookies92@yahoo.com>
2014-04-02 16:28:33 -0400

    changed map maker for linux compatibility

--- MapMaker/colors.c
+++ MapMaker/colors.c
@@ -16,9 +16,9 @@ int main(int argc, char *argv[])
  
 	int i, j;
 	int k = 0;
-	for(i = 0; i < 16; i++)
+	for(i = 0; i < 8; i++)
 	{
-		for(j = 0; j < 16; j++)
+		for(j = 0; j < 8; j++)
 		{
 			if(k)
 			{
